350a176eedf4878ed3a7370e10b9464551cd25b3
Rakki <165636034+RakkiZk@users.noreply.github.com>
2024-04-27 14:51:24 +0530

    config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,6 @@
 require('@nomicfoundation/hardhat-toolbox');
 
-const PRIVATE_KEY = '95cd7913d8db3bd75025e53e7774b8ece65578a1355453d8597e2877fe44d535'; 
+const PRIVATE_KEY = ''; 
 
 module.exports = {
   networks: {
